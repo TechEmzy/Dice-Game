bb33a444a1e81146fa9e69a93268d0a417630b70
TechEmzy <emmanuelemafe@gmail.com>
2023-11-29 22:01:44 +0100

    new commit

--- index.js
+++ index.js
@@ -16,29 +16,35 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //coverting lin
 
 document.querySelectorAll("img")[1].setAttribute("src"), randomImageSource2);*/
 
-// Create 2 varibles for random numbers from 1 to 6
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+function rollDice() {
+    // Create 2 varibles for random numbers from 1 to 6
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-// Select the classes for the images
-let img1 = document.querySelector(".img1")
-let img2 = document.querySelector(".img2")
+    // Select the classes for the images
+    let img1 = document.querySelector(".img1")
+    let img2 = document.querySelector(".img2")
 
-// Compare both img variables to each other and set the attributes
-// to access file and generate the random numbers to change actual file.
-let player1 = img1.setAttribute("src", "images/dice" + randomNumber1 + ".png")
-let player2 = img2.setAttribute("src", "images/dice" + randomNumber2 + ".png")
+    // Compare both img variables to each other and set the attributes
+    // to access file and generate the random numbers to change actual file.
+    let player1 = img1.setAttribute("src", "images/dice" + randomNumber1 + ".png")
+    let player2 = img2.setAttribute("src", "images/dice" + randomNumber2 + ".png")
 
-// Select h1 and chage winner of the game
-let winner = document.querySelector("h1")
+    // Select h1 and chage winner of the game
+    let winner = document.querySelector("h1")
 
 
-// Compare the randomNumber varibles and change to
-// Player 1 Wins! || Player 2 Wins! || Draw!
-if (randomNumber1 > randomNumber2) {
-    winner.innerHTML = "Player 1 Wins"
-} else if (randomNumber1 < randomNumber2) {
-    winner.innerHTML = "Player 2 Wins!"
-} else {
-    winner.innerHTML = "Draw!"
+    // Compare the randomNumber variables and change to
+    // Player 1 Wins! || Player 2 Wins! || Draw!
+    if (randomNumber1 > randomNumber2) {
+        winner.innerHTML = "Player 1 Wins"
+    } else if (randomNumber1 < randomNumber2) {
+        winner.innerHTML = "Player 2 Wins!"
+    } else {
+        winner.innerHTML = "Draw!"
+    }
 }
+
+// event listener to trigger the rollDice function
+document.getElementById("rollButton").addEventListener("click", rollDice);
+
